68c275b0b24453db9575922bbb5d8e49dc79075e
Oscar Jeschke <oscarjeschke@outlook.de>
2025-12-09 14:13:38 +0100

    added syntax website

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 function blackHoles() {
   window.location.href = "/projects/Black-Holes/index.html";
 }
+function syntaxRegeln() {
+  window.location.href =
+    "/projects/Syntax und Semantik Regeln/Syntax_Semantik_Regeln.html";
+}
